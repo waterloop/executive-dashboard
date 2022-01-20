8e7c9f34af00abe452bebb4e4569c67479c2e935
William Park <william.park@uwaterloo.ca>
2022-01-20 15:08:28 -0800

    Create the components for the landing page

--- src/pages/general/Landing/LandingPage.js
+++ src/pages/general/Landing/LandingPage.js
@@ -2,22 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 import Grid from '@material-ui/core/Grid';
 
-import RecruitmentSVG from '../../assets/recruitment-icon.svg';
-import AnalyticsSVG from '../../assets/analytics-icon.svg';
-import LandingCard from '../../components/LandingCard';
+import RecruitmentSVG from '../../../assets/svg/recruitment-icon.svg';
+import AnalyticsSVG from '../../../assets/svg/analytics-icon.svg';
 
-const Section = styled(LandingCard)`
-  margin-right: 56px;
-  margin-left: 56px;
-`;
+import LandingCard from '../../../components/LandingCard';
 
 const Container = styled.div`
-  display: flex;
-  justify-content: center;
-
-  padding-top: 96px;
-  padding-right: calc(min(128px, 10%));
-  padding-left: calc(min(128px, 10%));
+  margin: ${({ theme }) => theme.pageMargin};
 `;
 
 const sections = [
@@ -39,14 +30,14 @@ const sections = [
 
 const LandingPage = () => {
   const sectionItems = sections.map((section) => (
-    <Grid key={section.name} item xs={12} sm={6} justify="center">
-      <Section {...section} />
+    <Grid key={section.name} item>
+      <LandingCard {...section} />
     </Grid>
   ));
 
   return (
     <Container>
-      <Grid container justify="center">
+      <Grid container spacing={4} justifyContent="center" alignItems="center">
         {sectionItems}
       </Grid>
     </Container>
