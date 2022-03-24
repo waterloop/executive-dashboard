8839439329cd1fd07a55f52effde2f4a71d28aae
Lavan Sumanan <lavan.sumanan@gmail.com>
2022-03-23 20:21:04 -0400

    created sub-components for application profile page to be reusable

---
+++ src/components/ProfileTemplate/Header/Header.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import styled from 'styled-components';
+import Grid from '@material-ui/core/Grid';
+// import theme from '../../../theme';
+import backArrowIcon from '../../../assets/svg/recruitment/application/back-arrow.svg';
+
+const Container = styled.div`
+  margin: 0;
+`;
+
+const BackArrow = styled.button`
+  background: url(${backArrowIcon});
+  color: inherit;
+  border: none;
+  padding: 5;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+  position: fixed;
+  z-index: 2;
+  top: 4rem;
+  left: 1rem;
+  width: 1rem;
+  height: 1rem;
+  margin: 1rem 0;
+`;
+
+const Blobs = styled.img`
+  position: fixed;
+  width: ${({ width }) => width}
+  z-index: 2;
+  height: 13.438rem;
+  top: 4rem;
+  left: 11rem;
+`;
+
+const HeaderGrid = styled(Grid)`
+  width: 100%;
+  position: fixed;
+  z-index: 1;
+  padding: 64px 82px 10px 82px;
+  background: linear-gradient(91.05deg, #cad4ff 0%, #cef6ff 99.9%);
+`;
+
+const Name = styled.h1`
+  font: ${({ theme }) => theme.fonts.bold36};
+`;
+
+const DemographicText = styled.p`
+  font: ${({ theme }) => theme.fonts.medium20};
+`;
+
+// name is a string, current postings is an array of strings containing names of postings, blobs is the file path to an svg
+const Header = ({ name, currentPostings, blobs }) => {
+  const history = useHistory();
+
+  const handleBackClick = () => {
+    history.push('/recruitment/application');
+  };
+
+  return (
+    <Container>
+      <BackArrow onClick={() => handleBackClick()} />
+      <HeaderGrid item xs={12}>
+        <Blobs src={blobs} width="100%" />
+        <Name>{name}</Name>
+        <DemographicText>{currentPostings.join(' | ')}</DemographicText>
+      </HeaderGrid>
+    </Container>
+  );
+};
+
+export default Header;
---
+++ src/components/ProfileTemplate/Sidebar/Sidebar.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import styled from 'styled-components';
+import theme from '../../../theme';
+import DropdownMenu from '../../DropdownMenu';
+import resumeIcon from '../../../assets/svg/recruitment/application/resume-click.svg';
+
+const Container = styled.div`
+  margin: 0;
+`;
+
+const Title = styled.h2`
+  font: ${({ theme }) => theme.fonts.bold24};
+`;
+
+const DemographicText = styled.p`
+  font: ${({ theme }) => theme.fonts.medium20};
+`;
+
+const DemographicLink = styled.a`
+  font: ${({ theme }) => theme.fonts.medium20};
+  color: ${({ theme }) => theme.colours.blues.blue4};
+  text-decoration: none;
+`;
+
+// program is a string
+// term is a string
+// resumeLink is a string containing a link
+// initialStatus is a string representing the initial status to display on the status dropdown menu
+const Sidebar = ({ program, term, resumeLink, initialStatus }) => {
+  const options = [
+    'Pending',
+    'Rejected',
+    'Undecided',
+    'Interview Set Up',
+    'Interview Pending',
+    'Interview Rejected',
+    'Interview Undecided',
+    'Accepted',
+  ];
+
+  const backgrounds = [
+    theme.colours.yellows.yellow1,
+    theme.colours.reds.red1,
+    theme.colours.greys.grey2,
+    theme.colours.blues.blue2,
+    theme.colours.yellows.yellow1,
+    theme.colours.reds.red1,
+    theme.colours.greys.grey2,
+    theme.colours.greens.green2,
+  ];
+
+  const statuses = {
+    app_pending: 'Pending',
+    app_reject: 'Rejected',
+    app_undecided: 'Undecided',
+    interview_setup: 'Interview Set Up',
+    interview_pending: 'Interview Pending',
+    interview_reject: 'Interview Rejected',
+    interview_undecided: 'Interview Undecided',
+    final_accept: 'Accepted',
+  };
+
+  return (
+    <Container>
+      <Title>Demographic</Title>
+      <DemographicText>{program}</DemographicText>
+      <DemographicText>{term}</DemographicText>
+      <DemographicLink target="_blank" rel="noreferrer" href={resumeLink}>
+        Resume <img alt="click" src={resumeIcon} />
+      </DemographicLink>
+      <Title>Status</Title>
+      <DemographicText>Current Status</DemographicText>
+      <DropdownMenu
+        options={options}
+        backgrounds={backgrounds}
+        initialStatus={statuses[initialStatus]}
+      />
+    </Container>
+  );
+};
+
+export default Sidebar;
