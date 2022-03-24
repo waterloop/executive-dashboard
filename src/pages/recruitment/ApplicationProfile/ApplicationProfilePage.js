cf88070a9031609c02d679736da3926080a4971a
Lavan Sumanan <lavan.sumanan@gmail.com>
2022-03-23 20:21:04 -0400

    created sub-components for application profile page to be reusable

--- src/pages/recruitment/ApplicationProfile/ApplicationProfilePage.js
+++ src/pages/recruitment/ApplicationProfile/ApplicationProfilePage.js
@@ -1,50 +1,14 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import Grid from '@material-ui/core/Grid';
-import DropDownMenu from '../../../components/DropdownMenu';
 import theme from '../../../theme';
-import resumeIcon from '../../../assets/svg/recruitment/application/resume-click.svg';
-import backArrowIcon from '../../../assets/svg/recruitment/application/back-arrow.svg';
 import blobs from '../../../assets/svg/recruitment/application/blobs.svg';
+
+import Header from '../../../components/ProfileTemplate/Header';
+import Sidebar from '../../../components/ProfileTemplate/Sidebar';
 
 const Container = styled.div`
   margin: 0;
-`;
-
-const BackArrow = styled.button`
-  background: url(${backArrowIcon});
-  color: inherit;
-  border: none;
-  padding: 5;
-  font: inherit;
-  cursor: pointer;
-  outline: inherit;
-  position: fixed;
-  z-index: 2;
-  top: 4rem;
-  left: 1rem;
-  width: 1rem;
-  height: 1rem;
-  margin: 1rem 0;
-`;
-
-const Blobs = styled.img`
-  position: fixed;
-  width: ${({ width }) => width}
-  z-index: 2;
-  src: ${blobs};
-  height: 13.438rem;
-  top: 4rem;
-  left: 11rem;
-`;
-
-const HeaderGrid = styled(Grid)`
-  width: 100%;
-  position: fixed;
-  z-index: 1;
-  padding: 64px 82px 10px 82px;
-  background: linear-gradient(91.05deg, #cad4ff 0%, #cef6ff 99.9%);
 `;
 
 const ContentGrid = styled(Grid)`
@@ -58,26 +22,12 @@ const PostingGrid = styled(Grid)`
   z-index: 0;
 `;
 
-const Name = styled.h1`
-  font: ${({ theme }) => theme.fonts.bold36};
-`;
-
 const Title = styled.h2`
   font: ${({ theme }) => theme.fonts.bold24};
 `;
 
 const Subtitle = styled.h2`
   font: ${({ theme }) => theme.fonts.bold18};
-`;
-
-const DemographicText = styled.p`
-  font: ${({ theme }) => theme.fonts.medium20};
-`;
-
-const DemographicLink = styled.a`
-  font: ${({ theme }) => theme.fonts.medium20};
-  color: ${({ theme }) => theme.colours.blues.blue4};
-  text-decoration: none;
 `;
 
 const ApplicationText = styled.p`
@@ -109,8 +59,6 @@ const PostingButton = styled.button`
 `;
 
 const ApplicationProfilePage = () => {
-  const history = useHistory();
-
   const mockData = {
     name: 'John Smith',
     demographic: {
@@ -129,20 +77,28 @@ Voluptate eiusmod duis veniam ipsum est qui officia voluptate quis. Eu proident 
 
 Do est cillum laborum commodo proident magna do ullamco aliquip quis aliqua dolor proident. Voluptate ipsum excepteur velit ullamco commodo voluptate ex ad dolore qui enim. Duis eu proident anim fugiat Lorem reprehenderit dolore officia incididunt in. Aute sit nostrud cillum labore eu irure sit ea id dolor deserunt. Magna et consectetur est incididunt aliquip ullamco sunt non proident consequat ea proident aliqua qui.
 
-Ut magna irure eu nostrud irure ad ullamco exercitation sint. Ad irure eu eu Lorem adipisicing. Pariatur laborum irure elit consequat elit. Veniam pariatur do elit irure fugiat velit amet ea. Eu nisi ad veniam id duis officia aute Lorem cupidatat.`,
+Ut magna irure eu nostrud irure ad ullamco exercitation sint. Ad irure eu eu Lorem adipisicing. Pariatur laborum irure elit consequat elit. Veniam pariatur do elit irure fugiat velit amet ea. Eu nisi ad veniam id duis officia aute Lorem cupidatat.
+Laborum excepteur labore reprehenderit aute aliqua veniam. Ipsum qui irure voluptate do fugiat Lorem minim cupidatat irure quis pariatur labore elit eu. Ut eiusmod aliqua nisi in sint deserunt sint proident excepteur esse magna. Do laborum qui consequat laborum Lorem ipsum cillum ullamco Lorem mollit esse et enim. Occaecat qui quis aute proident reprehenderit ut sit commodo et sit dolor fugiat occaecat.
+
+Nisi cupidatat ut culpa eiusmod magna dolor et. Anim eiusmod minim in nostrud tempor. Ullamco sit ullamco adipisicing dolor exercitation mollit consequat sunt laborum nisi magna est aute. Officia aliqua commodo sint reprehenderit non ipsum.
+
+Ea pariatur dolore ad anim non ea pariatur reprehenderit in laborum est duis. Consectetur amet duis veniam qui voluptate aliquip do dolore. Sit aliquip nostrud minim reprehenderit commodo ut labore velit cupidatat dolor nulla. Pariatur esse ut mollit esse. Id deserunt aliqua Lorem dolore veniam fugiat aute laboris occaecat culpa Lorem eiusmod. Anim cillum voluptate irure aliqua aliqua qui sint elit.
+
+Reprehenderit eiusmod occaecat ipsum deserunt. Officia non mollit quis eiusmod voluptate tempor occaecat. Nisi non eiusmod occaecat veniam ea ea aute sunt pariatur esse culpa duis.
+
+Exercitation esse anim minim ad commodo sunt deserunt aute nulla. Anim nisi qui id deserunt non dolore esse consequat in officia in consectetur. Non eu in culpa enim amet. Excepteur consectetur magna incididunt culpa culpa laboris enim velit duis aute. Tempor minim reprehenderit sint nisi eu.
+
+Irure dolor culpa duis et amet anim Lorem enim aliquip quis. Est quis excepteur ullamco ipsum laboris ea pariatur reprehenderit. Proident et qui culpa ipsum deserunt consectetur culpa magna aliquip esse.
+
+Pariatur ea anim nulla aute aliqua quis do adipisicing. Sit incididunt cupidatat consequat reprehenderit aliquip deserunt exercitation do sunt non anim ullamco. Cupidatat elit eu id ipsum est.
+
+Anim officia officia sint ea. Enim occaecat elit elit tempor irure veniam in labore Lorem reprehenderit consectetur pariatur. Commodo laboris exercitation reprehenderit aliquip reprehenderit aliquip mollit esse. Duis aute dolor id amet sint commodo ut occaecat cupidatat.
+
+Excepteur minim deserunt quis laborum do eu. Ullamco commodo laboris do commodo ut occaecat aute. Lorem consequat elit duis cupidatat aute qui eiusmod amet aliqua ullamco veniam minim. Veniam duis eu ex id id laborum officia non ex consectetur ipsum.
+
+Id reprehenderit ad mollit mollit. Consequat in et occaecat dolor aliquip esse adipisicing. Excepteur consequat consequat occaecat commodo sunt elit anim reprehenderit irure labore. Qui non nostrud magna cillum adipisicing in in. Amet ad pariatur officia laboris irure sit.`,
     currentPostings: ['UI/UX Design', 'Frontend Dev', 'Fullstack Dev'],
     previousPostings: ['UI/UX Design', 'Fullstack Dev'],
-  };
-
-  const statuses = {
-    app_pending: 'Pending',
-    app_reject: 'Rejected',
-    app_undecided: 'Undecided',
-    interview_setup: 'Interview Set Up',
-    interview_pending: 'Interview Pending',
-    interview_reject: 'Interview Rejected',
-    interview_undecided: 'Interview Undecided',
-    final_accept: 'Accepted',
   };
 
   const postingColours = {
@@ -153,10 +109,6 @@ Ut magna irure eu nostrud irure ad ullamco exercitation sint. Ad irure eu eu Lor
 
   const handleClick = (e) => {
     console.log(e.target.textContent);
-  };
-
-  const handleBackClick = () => {
-    history.push('/recruitment/application');
   };
 
   const postingGenerator = (postings) =>
@@ -178,33 +130,21 @@ Ut magna irure eu nostrud irure ad ullamco exercitation sint. Ad irure eu eu Lor
 
   return (
     <Container>
-      <BackArrow onClick={() => handleBackClick()} />
-      {/* entire page */}
-      {/* header takes up 1/4 of the height */}
-      <HeaderGrid item xs={12}>
-        <Blobs src={blobs} width="100%" />
-        <Name>{mockData.name}</Name>
-        <DemographicText>
-          {mockData.currentPostings.join(' | ')}
-        </DemographicText>
-      </HeaderGrid>
+      <Header
+        name={mockData.name}
+        currentPostings={mockData.currentPostings}
+        blobs={blobs}
+      />
       {/* application info takes up 3/4 of height */}
       <ContentGrid justifyContent="flex-start" item xs={12} container>
         {/* sidebar takes up 1/3 of width */}
         <Grid item xs={12} md={4}>
-          <Title>Demographic</Title>
-          <DemographicText>{mockData.demographic.program}</DemographicText>
-          <DemographicText>{mockData.demographic.term}</DemographicText>
-          <DemographicLink
-            target="_blank"
-            rel="noreferrer"
-            href={mockData.demographic.resumeLink}
-          >
-            Resume <img alt="click" src={resumeIcon} />
-          </DemographicLink>
-          <Title>Status</Title>
-          <DemographicText>Current Status</DemographicText>
-          <DropDownMenu initialStatus={statuses[mockData.status]} />
+          <Sidebar
+            program={mockData.demographic.program}
+            term={mockData.demographic.term}
+            resumeLink={mockData.demographic.resumeLink}
+            initialStatus={mockData.status}
+          />
         </Grid>
         {/* main content takes up 2/3 of width */}
         <Grid item xs={12} md={8}>
