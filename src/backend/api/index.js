a5dffbcb5b7c96da4593c52ebc47a459278a9d52
sashco mistelbacher <sashco.mistelbacher@gmail.com>
2022-10-19 21:19:12 -0400

    basic file uploader wip
    fix pkg.json conflict

--- src/backend/api/index.js
+++ src/backend/api/index.js
@@ -3,11 +3,14 @@ import applications from './applications';
 import interviews from './interviews';
 import configuration from './configuration';
 import email from './email';
+import upload from './upload';
 
 const router = express.Router();
 router.use('/applications', applications);
 router.use('/interviews', interviews);
 router.use('/configuration', configuration);
 router.use('/email', email);
+router.use('/upload/resume', upload);
 
 export default router;
+
