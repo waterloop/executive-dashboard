42a5ec60c4a5e5a4d58f0e2ca0004f71b9891d0c
sashco mistelbacher <sashco.mistelbacher@gmail.com>
2022-10-29 15:31:34 -0400

    finished file upload and link return

--- src/backend/api/upload/add-resume.js
+++ src/backend/api/upload/add-resume.js
@@ -1,47 +1,111 @@
 // taken from
 //https://www.labnol.org/google-drive-api-upload-220412
-import { OAuth2Client, JWT } from 'google-auth-library';
+import { assert } from 'console';
+import { JWT } from 'google-auth-library';
 import {google} from 'googleapis';
+import { objOf } from 'ramda';
 import stream from 'stream';
 
-const uploadFile = async (fileObject) => {
-    const bufferStream = new stream.PassThrough();
-    bufferStream.end(fileObject.buffer);
-    // try with teamhub service acc
-    // permission error -> share file with service acc?
-    const jwtClient = new JWT({
-        scopes:["https://www.googleapis.com/auth/drive.file"],
+// get auth using the correct service account
+const getAuth = async () => {
+  return new JWT({
+        scopes:[
+          "https://www.googleapis.com/auth/drive.file",
+          "https://www.googleapis.com/auth/drive",
+          "https://www.googleapis.com/auth/drive.appdata",
+          "https://www.googleapis.com/auth/drive.metadata",
+        ],
         email:'teamhubbackend@teamhub-257722.iam.gserviceaccount.com',
         key: process.env.SERVICE_ACCOUNT_PRIVATE_KEY,
-        subject:"steven.x@waterloop.ca",
     })
+}
+
+// create the folder if it does not exist
+const putFolder = async (folderName, parentFolder, jwtClient) => {
+  const res = await google.drive({ version: 'v3', auth: jwtClient }).files.list({
+    q: `mimeType=\'application/vnd.google-apps.folder\' and \'${parentFolder}\' in parents and trashed=false`,
+    fields: 'nextPageToken, files(id,name)',
+    driveId:'0AAdrhjLJ8IiKUk9PVA',
+    corpora:'drive',
+    supportsAllDrives: true,
+    includeItemsFromAllDrives: true,
+  });
+  
+  let ret = res.data.files.find(f => f.name == folderName)
+
+  if(ret == undefined) {
+    // create new folder
+    const fileMetadata = {
+      name: folderName,
+      mimeType: "application/vnd.google-apps.folder",
+      parents: [parentFolder],
+    };
+    const { data } = await google.drive({ version: 'v3', auth: jwtClient }).files.create({
+      resource: fileMetadata,
+      fields:'id',
+      supportsAllDrives: true,
+    });
+    ret = data.id
+  } else {
+    // extract id field
+    ret = ret.id
+  }
+  return ret 
+}
+
+const uploadFile = async (fileObject, parentFolder, firstname, lastname, jwtClient) => {
+    // check for correct mime type (pdf)
+    // Serialize the file
+    const bufferStream = new stream.PassThrough();
+    bufferStream.end(fileObject.buffer);
     const { data } = await google.drive({ version: 'v3', auth: jwtClient }).files.create({
       media: {
-        mimeType: fileObject.mimeType,
+        mimeType: fileObject.mimetype,
         body: bufferStream,
       },
       requestBody: {
-        name: fileObject.originalname,
-        // hardcoded TEST folder
-        parents: ['1AAY9G_8OxKJ_vUTZoXbmcUxcl3Rqazsv'],
+        //name: fileObject.originalname,
+        name: `${firstname}_${lastname}.pdf`,
+        parents: [parentFolder],
       },
       fields: 'id,name',
+      supportsAllDrives: true,
     });
-    console.log(`Uploaded file ${data.name} ${data.id}`);
+
+    return data.id
   };
 
 const add = async (req, res) => {
     try {
         const { body, files } = req;
-
-        for (let f = 0; f < files.length; f += 1) {
-            await uploadFile(files[f]);
+        // do checks on body, files
+        const expected = ['firstname', 'lastname', 'term_year', 'subteam', 'position']
+        if(!expected.every(item => Object.prototype.hasOwnProperty.call(body, item)))
+          throw `Expect ${expected} in body`
+        if(!files.length == 1)
+          throw `Expect 1 file, recieved ${files.length}`
+        
+        // get auth client
+        const client = await getAuth()
+        // Determine which base folder to use
+        let parentFolder = '1BvULMQjVfhPw8cTCp3P80C7d6OKadpC_' // External/WEB DEV/TEST
+        if(process.env.NODE_ENV === 'production'){
+          parentFolder = '178nqXz6BCawIY09_o9JR0gI22EDJRujF' // External/Recruitment
         }
 
-        console.log(body);
-        res.status(200).send('Form Submitted');
-        } catch (f) {
-        res.send(f.message);
+        // put year folder
+        parentFolder = await putFolder(body.term_year, parentFolder, client)
+        // put resume folder
+        parentFolder = await putFolder("Resumes", parentFolder, client)
+        // put subteam folder
+        parentFolder = await putFolder(body.subteam, parentFolder, client)
+        // put position folder
+        parentFolder = await putFolder(body.position, parentFolder, client)
+        // upload file
+        let fileId = await uploadFile(files[0], parentFolder, body.firstname, body.lastname, client);
+        res.status(200).send(`https://drive.google.com/file/d/${fileId}/view?usp=sharing`);
+     } catch (f) {
+        res.status(400).send(f);
     }
 }
 
