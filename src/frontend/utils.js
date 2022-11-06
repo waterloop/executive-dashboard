d34cdcbf774d9222960d2ae10b0b3cfb96d7ec50
Brian Gao <bhgao20014@gmail.com>
2022-11-06 12:41:08 -0500

    Improve utils documentation

--- src/frontend/utils.js
+++ src/frontend/utils.js
@@ -103,7 +103,6 @@ export const getTermDate = (date) =>
 /**
  * Creates data from an array of values:
  */
-
 export const createData = (keys, values) => {
   const res = {};
   keys.forEach((kval, idx) => {
@@ -120,24 +119,32 @@ export const getItemById = (arr, id) => {
   return obj[0];
 };
 
-// Converts a camelCase string to its snake_case equivalent
-// e.g., importantObjectProperty -> important_object_property
+/**
+ * Converts a camelCase string to its snake_case equivalent
+ * e.g., importantObjectProperty -> important_object_property
+ */
 export const camelCaseToSnakeCase = (s) =>
   s
     .split(/(?=[A-Z])/)
     .join('_')
     .toLowerCase();
 
-// Converts a snake_case string to its camelCase equivalent
-// e.g., important_object_property -> importantObjectProperty
+/**
+ * Converts a snake_case string to its camelCase equivalent
+ * e.g., important_object_property -> importantObjectProperty
+ */
 export const snakeCaseToCamelCase = (s) =>
   s.replace(/[-_][a-z]/g, (group) => group.slice(-1).toUpperCase());
 
-// Converts Date object to string in the format '<year>-<month>-<day>'
+/**
+ * Converts Date object to string in the format '<month>-<day>-<year>'
+ */
 export const dateToString = (date) =>
   `${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()}`;
 
-// Converts time (represented as Date object) to string in the format '<hour>:<minute> <AM/PM>'
+/**
+ * Converts time (represented as Date object) to string in the format '<hour>:<minute> <AM/PM>'
+ */
 export const timeToString = (time) =>
   time.toLocaleString('en-US', {
     hour: 'numeric',
@@ -145,10 +152,15 @@ export const timeToString = (time) =>
     hour12: true,
   });
 
-// Converts string in the format '<year>-<month>-<day>' to Date Object
+/**
+ * Converts string in the format '<month>-<day>-<year>' to Date Object
+ */
 export const stringToDate = (s) => new Date(s);
 
-// Converts string in the format '<hour>:<minute> <AM/PM>' to Date Object
+/**
+ * Converts string in the format '<hour>:<minute> <AM/PM>' to Date Object
+ * Ref: https://www.freecodecamp.org/news/mathematics-converting-am-pm-to-24-hour-clock
+ */
 export const stringToTime = (s) => {
   const period = s.slice(-2) === 'AM' ? 'AM' : 'PM';
   // eslint-disable-next-line prefer-const
@@ -159,6 +171,7 @@ export const stringToTime = (s) => {
     hour = (parseInt(hour, 10) + 12).toString();
   }
   const currentDate = new Date();
+  // Year, month, and day attributes don't matter for time, so it's set to current year, month, and day
   return new Date(
     currentDate.getFullYear(),
     currentDate.getMonth(),
