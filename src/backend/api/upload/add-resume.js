656a000d2038e965a2b7a280ff4231579fa7dc9e
sashco mistelbacher <sashco.mistelbacher@gmail.com>
2022-11-14 22:49:31 -0500

    improve comments

--- src/backend/api/upload/add-resume.js
+++ src/backend/api/upload/add-resume.js
@@ -4,7 +4,15 @@ import { JWT } from 'google-auth-library';
 import {google} from 'googleapis';
 import stream from 'stream';
 
-// get auth using the correct service account
+// Move to dedicated constants file when needed again
+const ExternalDriveID = '0AAdrhjLJ8IiKUk9PVA'
+const ExternalDev = '1BvULMQjVfhPw8cTCp3P80C7d6OKadpC_' // External/WEB DEV/TEST
+const ExternalProd = '178nqXz6BCawIY09_o9JR0gI22EDJRujF' // External/Recruitment
+
+/**
+ * get auth client using the correct service account
+ * @returns Google Auth Client
+ */
 const getAuth = async () => {
   return new JWT({
         scopes:[
@@ -18,12 +26,19 @@ const getAuth = async () => {
     })
 }
 
-// create the folder if it does not exist
+/**
+ * 
+ * Create the folder if it does not exist
+ * @param {String} folderName the name of the folder
+ * @param {String} parentFolder the parent folder ID
+ * @param {JWT} jwtClient the Google Auth Client returned by getAuth
+ * @returns ID of created folder
+ */
 const putFolder = async (folderName, parentFolder, jwtClient) => {
   const res = await google.drive({ version: 'v3', auth: jwtClient }).files.list({
     q: `mimeType=\'application/vnd.google-apps.folder\' and \'${parentFolder}\' in parents and trashed=false`,
     fields: 'nextPageToken, files(id,name)',
-    driveId:'0AAdrhjLJ8IiKUk9PVA',
+    driveId: ExternalDriveID,
     corpora:'drive',
     supportsAllDrives: true,
     includeItemsFromAllDrives: true,
@@ -51,7 +66,16 @@ const putFolder = async (folderName, parentFolder, jwtClient) => {
   return ret 
 }
 
-const uploadFile = async (fileObject, parentFolder, firstname, lastname, jwtClient) => {
+/**
+ * 
+ * Uploads the provided file
+ * @param {Object} fileObject the file from the request
+ * @param {String} parentFolder the ID of the parent folder
+ * @param {String} fileName the name of the file
+ * @param {JWT} jwtClient the client returned by getAuth
+ * @returns 
+ */
+const uploadFile = async (fileObject, parentFolder, fileName, jwtClient) => {
     // check for correct mime type (pdf)
     // Serialize the file
     const bufferStream = new stream.PassThrough();
@@ -62,8 +86,7 @@ const uploadFile = async (fileObject, parentFolder, firstname, lastname, jwtClie
         body: bufferStream,
       },
       requestBody: {
-        //name: fileObject.originalname,
-        name: `${firstname}_${lastname}.pdf`,
+        name: fileName,
         parents: [parentFolder],
       },
       fields: 'id,name',
@@ -86,9 +109,9 @@ const add = async (req, res) => {
         // get auth client
         const client = await getAuth()
         // Determine which base folder to use
-        let parentFolder = '1BvULMQjVfhPw8cTCp3P80C7d6OKadpC_' // External/WEB DEV/TEST
+        let parentFolder = ExternalDev
         if(process.env.NODE_ENV === 'production'){
-          parentFolder = '178nqXz6BCawIY09_o9JR0gI22EDJRujF' // External/Recruitment
+          parentFolder = ExternalProd
         }
 
         // put year folder
@@ -100,7 +123,11 @@ const add = async (req, res) => {
         // put position folder
         parentFolder = await putFolder(body.position, parentFolder, client)
         // upload file
-        let fileId = await uploadFile(files[0], parentFolder, body.firstname, body.lastname, client);
+        let fileId = await uploadFile(files[0], 
+          parentFolder, 
+          `${body.firstname}_${body.lastname}.pdf`, 
+          client);
+        // send response with google drive linke
         res.status(200).send(`https://drive.google.com/file/d/${fileId}/view?usp=sharing`);
      } catch (f) {
         res.status(400).send(f);
